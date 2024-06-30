ec841124eac18396af6698528f3b854ac55c876c
silkwave <47917721+silkwave@users.noreply.github.com>
2024-06-30 16:14:06 +0900

    Delete ex02/target directory

--- ex02/target/debug/deps/ex02-f3d14991fe3248cf.d
+++
@@ -1,5 +0,0 @@
-/home/silkwave/apps/github/rust/ex02/target/debug/deps/ex02-f3d14991fe3248cf: src/main.rs
-
-/home/silkwave/apps/github/rust/ex02/target/debug/deps/ex02-f3d14991fe3248cf.d: src/main.rs
-
-src/main.rs:
