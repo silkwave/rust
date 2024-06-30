f407c94735104413ec0ed9f2cf62440c42741e61
silkwave <47917721+silkwave@users.noreply.github.com>
2024-06-30 16:24:38 +0900

    Delete ex04/target directory

--- ex04/target/debug/deps/ex04-5df6b75ad48df7a3.d
+++
@@ -1,5 +0,0 @@
-/home/silkwave/apps/github/rust/ex04/target/debug/deps/ex04-5df6b75ad48df7a3: src/main.rs
-
-/home/silkwave/apps/github/rust/ex04/target/debug/deps/ex04-5df6b75ad48df7a3.d: src/main.rs
-
-src/main.rs:
--- ex04/target/debug/ex04.d
+++
@@ -1,1 +0,0 @@
-/home/silkwave/apps/github/rust/ex04/target/debug/ex04: /home/silkwave/apps/github/rust/ex04/src/main.rs
